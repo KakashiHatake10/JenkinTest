9f7db91e13a957dcf52406b630ce7cb645357a10
Kakashi <62841867+KakashiHatake10@users.noreply.github.com>
2022-08-25 23:47:52 +0530

    initial commit

--- src/main/java/com/example/demo/Mini1Application.java
+++ src/main/java/com/example/demo/Mini1Application.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Mini1Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Mini1Application.class);
+
+	@PostConstruct
+	public void init() {
+		log.info("applicatipon start");
+	}
+
 	public static void main(String[] args) {
+		log.info("inside main...............");
 		SpringApplication.run(Mini1Application.class, args);
 	}
 
